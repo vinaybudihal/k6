389264aef803796234953a17f4d38d726b588de6
vinaybudihal <131458526+vinaybudihal@users.noreply.github.com>
2024-01-18 19:51:21 +0530

    Update and rename test4.js to satellite_service_health.js

--- satellite_service_health.js
+++ satellite_service_health.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-  vus: 1000,
-  duration: '120s'
+  vus: 10000,
+  duration: '240s'
 };
 
 export default function () {
-  const result = http.get('https://test-api.k6.io/public/crocodiles/');
+  const result = http.get('https://omcs-ssp2-pre-dev.opc.oracleoutsourcing.com/satellite-service/health');
   check(result, {
     'http response status code is 200': result.status === 200,
   });
