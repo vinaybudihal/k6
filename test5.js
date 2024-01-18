67a756f5beffa1491be347a3413bc3f1ac33cea8
vinaybudihal <131458526+vinaybudihal@users.noreply.github.com>
2024-01-18 19:20:20 +0530

    Update test5.js

--- test5.js
+++ test5.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const testConfig = JSON.parse(open('./test-config.json'));
+const testConfig = JSON.parse(open('./config.json'));
 
 const discoverIssuer = async (issuerUrl) => {
     const url = issuerUrl + '/.well-known/openid-configuration';
